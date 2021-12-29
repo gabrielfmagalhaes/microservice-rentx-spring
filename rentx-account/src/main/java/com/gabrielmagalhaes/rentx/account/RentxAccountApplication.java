e02121ecc249acc4565ac13eed2c5dd1c2b163ad
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2021-12-29 19:15:27 -0300

    add rentx account microservice

---
+++ rentx-account/src/main/java/com/gabrielmagalhaes/rentx/account/RentxAccountApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielmagalhaes.rentx.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentxAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentxAccountApplication.class, args);
+    }
+
+}
---
+++ rentx-account/src/test/java/com/gabrielmagalhaes/rentx/account/RentxAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielmagalhaes.rentx.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentxAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
