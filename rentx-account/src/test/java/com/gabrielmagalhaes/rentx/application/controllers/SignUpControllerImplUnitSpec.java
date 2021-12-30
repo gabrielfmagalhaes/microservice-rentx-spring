9b2f982847e4e9985676225b412ad8573dce0135
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2021-12-30 06:19:06 -0300

    test: ensure that SignupController adds an account

--- rentx-account/src/test/java/com/gabrielmagalhaes/rentx/application/controllers/SignUpControllerImplUnitSpec.java
+++ rentx-account/src/test/java/com/gabrielmagalhaes/rentx/application/controllers/SignUpControllerImplUnitSpec.java
@@ -1,15 +1,25 @@
 package com.gabrielmagalhaes.rentx.application.controllers;
 
-import com.gabrielmagalhaes.rentx.domain.contracts.gateways.SignUpRequest;
+import com.gabrielmagalhaes.rentx.core.domain.Account;
+import com.gabrielmagalhaes.rentx.core.usecases.AddAccountUseCase;
+import com.gabrielmagalhaes.rentx.data.entities.SignUpRequest;
+import com.gabrielmagalhaes.rentx.data.usecases.AddAccountUseCaseImpl;
+import com.gabrielmagalhaes.rentx.presenter.SignUpController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+
 
 @RunWith(SpringRunner.class)
 @WebFluxTest(SignUpController.class)
@@ -17,6 +27,9 @@ public class SignUpControllerImplUnitSpec {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @MockBean
+    private AddAccountUseCase addAccountUseCase;
 
     @Test
     @DisplayName("should return 400 if email is empty")
@@ -29,7 +42,28 @@ public class SignUpControllerImplUnitSpec {
                 .accept(MediaType.APPLICATION_JSON)
                 .body(Mono.just(request), SignUpRequest.class)
                 .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody().jsonPath("$.data").isEqualTo("email must not be blank");
+                .expectStatus().isBadRequest();
+    }
+
+    @Test
+    @DisplayName("should return 201 if valid data is provided ")
+    void shouldReturnCreatedWhenValidDataIsProvided() {
+        SignUpRequest request = SignUpRequest.builder()
+                .name("valid_name")
+                .email("valid_email@email.com")
+                .password("valid_password")
+                .passwordConfirmation("valid_password")
+                .admin(true)
+                .build();
+
+        BDDMockito.when(addAccountUseCase.add(any(Account.class))).thenReturn(null);
+
+        webTestClient.post()
+                .uri("/signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), SignUpRequest.class)
+                .exchange()
+                .expectStatus().isCreated();
     }
 }
