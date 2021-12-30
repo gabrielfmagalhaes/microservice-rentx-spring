274e404e40967993ae811d507bde09c0a547afc9
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2021-12-30 14:50:53 -0300

    feat: add field match validation

--- rentx-account/src/main/java/com/gabrielmagalhaes/rentx/data/entities/SignUpRequest.java
+++ rentx-account/src/main/java/com/gabrielmagalhaes/rentx/data/entities/SignUpRequest.java
@@ -1,6 +1,9 @@
 package com.gabrielmagalhaes.rentx.data.entities;
 
+import com.gabrielmagalhaes.rentx.core.domain.Account;
+import com.gabrielmagalhaes.rentx.validation.constraint.FieldMatch;
 import lombok.*;
+import reactor.core.publisher.Mono;
 
 import javax.validation.constraints.NotBlank;
 
@@ -8,6 +11,8 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@With
+@FieldMatch(first = "password", second = "passwordConfirmation", message = "password fields must match")
 public class SignUpRequest {
 
     @NotBlank
@@ -23,4 +28,8 @@ public class SignUpRequest {
     private String passwordConfirmation;
 
     private boolean admin;
+
+    public static Account toDomain(SignUpRequest request) {
+        return new Account(request.name, request.email, request.password, request.admin);
+    }
 }
---
+++ rentx-account/src/main/java/com/gabrielmagalhaes/rentx/validation/constraint/FieldMatchValidator.java
@@ -0,0 +1,38 @@
+package com.gabrielmagalhaes.rentx.validation.constraint;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.PropertyAccessorFactory;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
+
+    private String firstFieldName;
+    private String secondFieldName;
+    private String message;
+
+    @Override
+    public void initialize(final FieldMatch constraintAnnotation) {
+        firstFieldName = constraintAnnotation.first();
+        secondFieldName = constraintAnnotation.second();
+        message = constraintAnnotation.message();
+    }
+
+    @Override
+    public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+        final Object firstObj = PropertyAccessorFactory.forBeanPropertyAccess(value).getPropertyValue(firstFieldName);
+        final Object secondObj = PropertyAccessorFactory.forBeanPropertyAccess(value).getPropertyValue(secondFieldName);
+
+        boolean valid =  firstObj == null && secondObj == null || firstObj != null && firstObj.equals(secondObj);
+
+        if (!valid){
+            context.buildConstraintViolationWithTemplate(message)
+                    .addPropertyNode(firstFieldName)
+                    .addConstraintViolation()
+                    .disableDefaultConstraintViolation();
+        }
+
+        return valid;
+    }
+}
